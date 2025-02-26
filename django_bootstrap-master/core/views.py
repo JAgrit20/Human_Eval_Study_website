a1ca05830337f43388610f067edd59a898c47368
Jagrit Acharya <jagritacharya2020@gmail.com>
2025-02-25 23:50:25 -0700

    first commit

---
+++ django_bootstrap-master/core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html', {})
+def examples(request):
+    return render(request, 'examp.html', {})
+def team(request):
+    return render(request, 'team.html', {})
