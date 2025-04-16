fe3a869953476ec5a96b66333271833d1583f892
Jagrit Acharya <jagritacharya2020@gmail.com>
2025-04-15 19:20:44 -0600

    mc

--- django_bootstrap-master/core/settings.py
+++ django_bootstrap-master/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '*un#_m)yi-jko&r(j_7133i$)@yu2zj%rdymvu2hjr5b4ax3w-'
 DEBUG = True
 ALLOWED_HOSTS = ['experiment.jobmatchify.com','3.210.239.32', 'localhost', '127.0.0.1','experiment.shaktilab.org','44.204.46.126']
 
-
+USE_TZ = False
 # Application definition
 
 INSTALLED_APPS = [
